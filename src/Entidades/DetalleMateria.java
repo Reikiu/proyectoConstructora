8f2ac0dc08c4b6e88bfce4ddfb4909f2237248e6
ang14ped16 <ang14ped16@DESKTOP-L8VA1OP>
2019-09-03 22:10:20 -0600

    clases detalles

---
+++ src/Entidades/DetalleMateria.java
@@ -0,0 +1,47 @@
+package Entidades;
+
+/**
+ *
+ * @author ang14ped16
+ */
+public class DetalleMateria {
+    private int idProyecto;
+    private int idMateria;
+    private double cantidad;
+
+    public DetalleMateria() {
+    }
+
+    public DetalleMateria(int idProyecto, int idMateria, double cantidad) {
+        this.idProyecto = idProyecto;
+        this.idMateria = idMateria;
+        this.cantidad = cantidad;
+    }
+
+    public int getIdProyecto() {
+        return idProyecto;
+    }
+
+    public void setIdProyecto(int idProyecto) {
+        this.idProyecto = idProyecto;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+    
+}
