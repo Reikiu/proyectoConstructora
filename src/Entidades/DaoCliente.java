90122af7acbafe8f52e7735f15a4bb72af965022
aalej <aalej@DESKTOP-RT8OC0H>
2019-09-05 23:55:18 -0600

    finalizado crud de cliente

--- src/Entidades/DaoCliente.java
+++ src/Entidades/DaoCliente.java
@@ -7,7 +7,10 @@
 package Entidades;
 import Conexion.Conexion;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 
@@ -20,9 +23,21 @@ public class DaoCliente extends Conexion{
     public void insertarCLiente(Cliente cli) throws Exception{
         try {
             this.conectar();
-            String sql="insert into clientes values (?,?,?,?,?,?)";
+            ResultSet rs1;
+            String sql1 = "Select * from cliente";
+            PreparedStatement pst1 = this.getCon().prepareStatement(sql1);
+            rs1 = pst1.executeQuery();
+            int i = 0;
+            while (rs1.next()) {
+                if (rs1.getInt("idCliente") > i) {
+                    i = rs1.getInt("idCliente");
+                }
+            }
+            i = i + 1;
+            
+            String sql="insert into cliente values (?,?,?,?,?,?)";
             PreparedStatement pre=this.getCon().prepareStatement(sql);
-            pre.setInt(1, cli.getIdCliente());
+            pre.setInt(1, i);
             pre.setString(2, cli.getNombre());
             pre.setString(3, cli.getDireccion());
             pre.setString(4, cli.getDui());
@@ -34,44 +49,74 @@ public class DaoCliente extends Conexion{
                     +e.getMessage());
         }
     }
-    public void modificarCliente(Cliente cli) throws Exception{
+    
+    public void modificarCliente(Cliente cli) throws SQLException {
         try {
             this.conectar();
-            String sql="update cliente set nombre='?', direccion='?', dui='?', "
-                    + "fechaNacimiento='?', telefono='?' where idCliente='?'";
-            PreparedStatement pre=this.getCon().prepareStatement(sql);
-            pre.setString(1, cli.getNombre());
-            pre.setString(2, cli.getDireccion());
-            pre.setString(3, cli.getDui());
-            pre.setString(4, cli.getFechaNacimiento());
-            pre.setString(5, cli.getTelefono());
-            pre.setInt(6, cli.getIdCliente());
-            pre.executeUpdate();
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Error al modificar Cliente"+
-                    e.getMessage());
+
+            String sql = "Update cliente set nombre=?,direccion=?,dui=?,"
+                    + "fechaNacimiento=?,telefono=? where idCliente=?";
+            PreparedStatement pst = this.getCon().prepareStatement(sql);
+            pst.setInt(6, cli.getIdCliente());
+            pst.setString(1, cli.getNombre());
+            pst.setString(2, cli.getDireccion());
+            pst.setString(3, cli.getDui());
+            pst.setString(4, cli.getFechaNacimiento());
+            pst.setString(5, cli.getTelefono());
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Modificado exitosamente");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al intentar modificar" + e.toString());
         }
     }
-    public void eliminarCliente(Cliente cli) throws Exception{
-        try {
-            int var= JOptionPane.showConfirmDialog(null, "Seguro que desea eliminar el "
-                    + "registro?", "Confirmacion", JOptionPane.YES_NO_OPTION, 
-                    JOptionPane.QUESTION_MESSAGE);
-            if (var == JOptionPane.YES_OPTION) {
+    
+    public void eliminarCliente(Cliente cli) throws Exception {
+        int var = JOptionPane.showConfirmDialog(null, "Seguro que desea eliminar el "
+                + "registro?", "Confirmacion", JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE);
+        if (var == JOptionPane.YES_OPTION) {
+            try {
                 this.conectar();
-                String sql = "delete from cliente where idCliente='?'";
-                PreparedStatement pre = this.getCon().prepareStatement(sql);
-                pre.setInt(1, cli.getIdCliente());
-                pre.executeUpdate();
+                String sql = "Delete from cliente where idCliente=?";
+                PreparedStatement pst = this.getCon().prepareStatement(sql);
+                pst.setInt(1, cli.getIdCliente());
+                pst.executeUpdate();
+                JOptionPane.showMessageDialog(null, "El registro ha sido eliminado.");
+            } catch (SQLException e) {
+                JOptionPane.showConfirmDialog(null, "Error al intentar eliminar el "
+                        + "registro." + e.getMessage());
             }
-            else if(var==JOptionPane.NO_OPTION){
-                JOptionPane.showConfirmDialog(null, "Operacion cancelada");
-            }
-                
-        } catch (SQLException e) {
-            JOptionPane.showConfirmDialog(null, "Error al intentar eliminar el "
-                    + "registro."+e.getMessage());
+        } else if (var == JOptionPane.NO_OPTION) {
+            JOptionPane.showConfirmDialog(null, "Operacion cancelada", "Eliminado",
+                    JOptionPane.OK_OPTION);
         }
+    }
+    
+    public List<Cliente> mostrarCliente() throws Exception {
+        ResultSet rs;
+        List<Cliente> lstu = new ArrayList();
+
+        try {
+            this.conectar();
+            String sql = "Select * from cliente";
+            PreparedStatement pst = this.getCon().prepareStatement(sql);
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                Cliente cli = new Cliente();
+                cli.setIdCliente(rs.getInt("idCliente"));
+                cli.setNombre(rs.getString("nombre"));
+                cli.setDireccion(rs.getString("direccion"));
+                cli.setDui(rs.getString("dui"));
+                cli.setFechaNacimiento(rs.getString("fechaNacimiento"));
+                cli.setTelefono(rs.getString("telefono"));
+                lstu.add(cli);
+            }
+        } catch (Exception e) {
+            JOptionPane.showConfirmDialog(null, "Error " + e.toString());
+        } finally {
+            this.desconectar();
+        }
+        return lstu;
     }
     
 }
--- src/vistas/cliente.java
+++ src/vistas/cliente.java
@@ -4,6 +4,14 @@
  * and open the template in the editor.
  */
 package vistas;
+import Entidades.DaoCliente;
+import Entidades.Cliente;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -16,6 +24,84 @@ public class cliente extends javax.swing.JInternalFrame {
      */
     public cliente() {
         initComponents();
+        tablaCliente();
+    }
+    DaoCliente dao=new DaoCliente();
+    Cliente cli=new Cliente();
+    
+    public void tablaCliente(){
+        String[] columnas = {"Id", "Nombre", "Direccion", "DUI", "Fecha de "
+                + "Nacimiento", "Telefono"};
+        Object[] obj = new Object[6];
+        DefaultTableModel tabla=new DefaultTableModel(null, columnas);
+        List ls;
+        try {
+            ls = dao.mostrarCliente();
+            for (int i = 0; i < ls.size(); i++) {
+                cli = (Cliente) ls.get(i);
+                obj[0] = cli.getIdCliente();
+                obj[1] = cli.getNombre();
+                obj[2] = cli.getDireccion();
+                obj[3] = cli.getDui();
+                obj[4] = cli.getFechaNacimiento();
+                obj[5] = cli.getTelefono();
+                tabla.addRow(obj);
+            }
+            this.tablaCliente.setModel(tabla);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "No ok " + e.toString());
+        }
+    }
+    
+    public void crearCliente() throws Exception {
+        cli.setNombre(this.txtNombre.getText());
+        cli.setDireccion(this.txtDireccion.getText());
+        cli.setDui(this.txtDui.getText());
+        cli.setFechaNacimiento(this.txtFechaN.getText());
+        cli.setTelefono(this.txtTelefono.getText());
+        dao.insertarCLiente(cli);
+        limpiar();
+        tablaCliente();
+    }
+    
+    public void limpiar() {
+        this.txtNombre.setText("");
+        this.txtDireccion.setText("");
+        this.txtDui.setText("");
+        this.txtFechaN.setText("");
+        this.txtTelefono.setText("");
+    }
+    
+    public void modificar() throws SQLException{
+        int fila=this.tablaCliente.getSelectedRow();
+        cli.setIdCliente((int) this.tablaCliente.getValueAt(fila, 0));
+        cli.setNombre(this.txtNombre.getText());
+        cli.setDireccion(this.txtDireccion.getText());
+        cli.setDui(this.txtDui.getText());
+        cli.setFechaNacimiento(this.txtFechaN.getText());
+        cli.setTelefono(this.txtTelefono.getText());
+        dao.modificarCliente(cli);
+    }
+    
+    public void llenarTabla() {
+        try {
+            int fila = this.tablaCliente.getSelectedRow();
+            cli.setIdCliente((int) this.tablaCliente.getValueAt(fila, 0));
+            this.txtNombre.setText((String) this.tablaCliente.getValueAt(fila, 1));
+            this.txtDireccion.setText((String) this.tablaCliente.getValueAt(fila, 2));
+            this.txtDui.setText((String) this.tablaCliente.getValueAt(fila, 3));
+            this.txtFechaN.setText((String) this.tablaCliente.getValueAt(fila, 4));
+            this.txtTelefono.setText((String) this.tablaCliente.getValueAt(fila, 5));
+        } catch (Exception e) {
+            
+        }
+    }
+    
+    public void eliminar() throws SQLException, Exception {
+        int fila = this.tablaCliente.getSelectedRow();
+        cli.setIdCliente((int) this.tablaCliente.getValueAt(fila, 0));
+        dao.eliminarCliente(cli);
+        limpiar();
     }
 
     /**
@@ -37,7 +123,7 @@ public class cliente extends javax.swing.JInternalFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         txtCodigo = new javax.swing.JTextField();
-        txtNumbre = new javax.swing.JTextField();
+        txtNombre = new javax.swing.JTextField();
         txtDireccion = new javax.swing.JTextField();
         txtDui = new javax.swing.JTextField();
         txtFechaN = new javax.swing.JFormattedTextField();
@@ -65,7 +151,7 @@ public class cliente extends javax.swing.JInternalFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(261, Short.MAX_VALUE)
                 .addComponent(jLabel7)
                 .addGap(278, 278, 278))
         );
@@ -103,8 +189,10 @@ public class cliente extends javax.swing.JInternalFrame {
         jLabel6.setForeground(new java.awt.Color(0, 0, 102));
         jLabel6.setText("Telefono:");
 
+        txtCodigo.setEnabled(false);
+
         try {
-            txtFechaN.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("##/##/##")));
+            txtFechaN.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("####-##-##")));
         } catch (java.text.ParseException ex) {
             ex.printStackTrace();
         }
@@ -119,21 +207,41 @@ public class cliente extends javax.swing.JInternalFrame {
         jButton1.setFont(new java.awt.Font("Trebuchet MS", 1, 12)); // NOI18N
         jButton1.setForeground(new java.awt.Color(0, 102, 102));
         jButton1.setText("Guardar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setBackground(new java.awt.Color(204, 255, 255));
         jButton2.setFont(new java.awt.Font("Trebuchet MS", 1, 12)); // NOI18N
         jButton2.setForeground(new java.awt.Color(0, 102, 102));
         jButton2.setText("Modificar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setBackground(new java.awt.Color(204, 255, 255));
         jButton3.setFont(new java.awt.Font("Trebuchet MS", 1, 12)); // NOI18N
         jButton3.setForeground(new java.awt.Color(0, 102, 102));
         jButton3.setText("Eliminar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setBackground(new java.awt.Color(204, 255, 255));
         jButton4.setFont(new java.awt.Font("Trebuchet MS", 1, 12)); // NOI18N
         jButton4.setForeground(new java.awt.Color(0, 102, 102));
         jButton4.setText("Limpiar");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -148,23 +256,22 @@ public class cliente extends javax.swing.JInternalFrame {
                             .addComponent(jLabel5)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(txtFechaN))
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel2Layout.createSequentialGroup()
-                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel1)
-                                    .addComponent(jLabel2))
-                                .addGap(28, 28, 28)
-                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(txtCodigo)
-                                    .addComponent(txtNumbre, javax.swing.GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE)))
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel2Layout.createSequentialGroup()
-                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel3)
-                                    .addComponent(jLabel4))
-                                .addGap(22, 22, 22)
-                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(txtDireccion, javax.swing.GroupLayout.DEFAULT_SIZE, 82, Short.MAX_VALUE)
-                                    .addComponent(txtDui))))
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel1)
+                                .addComponent(jLabel2))
+                            .addGap(28, 28, 28)
+                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(txtCodigo)
+                                .addComponent(txtNombre, javax.swing.GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE)))
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel3)
+                                .addComponent(jLabel4))
+                            .addGap(22, 22, 22)
+                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(txtDireccion)
+                                .addComponent(txtDui)))
                         .addGroup(jPanel2Layout.createSequentialGroup()
                             .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                 .addComponent(jLabel6)
@@ -186,7 +293,7 @@ public class cliente extends javax.swing.JInternalFrame {
                 .addGap(18, 18, 18)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(txtNumbre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
@@ -227,6 +334,11 @@ public class cliente extends javax.swing.JInternalFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        tablaCliente.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tablaClienteMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tablaCliente);
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
@@ -234,8 +346,8 @@ public class cliente extends javax.swing.JInternalFrame {
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 452, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                .addContainerGap())
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -265,6 +377,50 @@ public class cliente extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        try {
+            // Boton Guardar
+            crearCliente();
+        } catch (Exception ex) {
+            Logger.getLogger(cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        tablaCliente();
+        limpiar();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        try {
+            // Boton modificar
+            modificar();
+        } catch (Exception ex) {
+            Logger.getLogger(cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        tablaCliente();
+        limpiar();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        try {
+            // Boton Eliminar
+            eliminar();
+        } catch (SQLException ex) {
+            Logger.getLogger(cliente.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        tablaCliente();
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void tablaClienteMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tablaClienteMouseClicked
+        // Evento de click en elemento de la tabla
+        llenarTabla();
+    }//GEN-LAST:event_tablaClienteMouseClicked
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // Boton Limpiar
+        limpiar();
+    }//GEN-LAST:event_jButton4ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
@@ -287,7 +443,7 @@ public class cliente extends javax.swing.JInternalFrame {
     private javax.swing.JTextField txtDireccion;
     private javax.swing.JTextField txtDui;
     private javax.swing.JFormattedTextField txtFechaN;
-    private javax.swing.JTextField txtNumbre;
+    private javax.swing.JTextField txtNombre;
     private javax.swing.JFormattedTextField txtTelefono;
     // End of variables declaration//GEN-END:variables
 }
